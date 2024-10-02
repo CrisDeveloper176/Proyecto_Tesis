b74da28d495b3062d10c240b847b11e52bb70c2d
CrisDeveloper176 <cristobal.fritz@virginiogomez.cl>
2024-10-02 12:10:21 -0600

    commit Inal

---
+++ gps_monitoring_project/GPS_Monitoring_App/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from .models import RastreoGPS
+
+# Create your views here.
+@login_required(login_url='/login/')
+def home(request):
+       return render(request, 'Home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('Home.html')  
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+    
+    return render(request, 'login.html')
+

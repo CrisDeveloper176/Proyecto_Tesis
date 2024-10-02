6b49514e25b4905b2c4c95497f71649a4935ad50
CrisDeveloper176 <cristobal.fritz@virginiogomez.cl>
2024-10-02 12:18:59 -0600

    ggfngdfnhgfd

--- gps_monitoring_project/GPS_Monitoring_App/urls.py
+++ gps_monitoring_project/GPS_Monitoring_App/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('', RedirectView.as_view(url= 'login', permanent =False)),
+    path('home/', views.home, name='home'),
     
     
 ]

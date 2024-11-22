e961b9b90b6aced3bdd75f55c43c4ae0ff0993d8
CrisDeveloper176 <cristobal.fritz@virginiogomez.cl>
2024-11-22 15:42:09 -0600

    organizacion1

--- gps_monitoring_project/GPS_Monitoring_App/views.py
+++ gps_monitoring_project/GPS_Monitoring_App/views.py
@@ -59,49 +59,7 @@ def login(request):
 
     return render(request, 'login.html')
 
-def register(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
 
-        # Verifica si los datos están vacíos
-        if not username or not email or not password:
-            return JsonResponse({'error': 'Todos los campos son obligatorios'}, status=400)
-
-        # Configura la URL del endpoint de la API
-        url = "http://apitesis.fly.dev/api/v1/Login/"  # Cambia la URL si es necesario
-
-        # Define los parámetros en formato JSON
-        data = {
-            "user": username,  # Reemplaza con 'username' si ese es el parámetro correcto
-            "email": email,
-            "password": password,
-        }
-
-        # Configura las cabeceras para enviar JSON
-        headers = {
-            "Content-Type": "application/json",
-        }
-
-        try:
-            # Realiza la solicitud POST con los datos en formato JSON
-            response = requests.post(url, data=json.dumps(data), headers=headers)
-
-            # Verifica si la solicitud fue exitosa
-            if response.status_code == 200:
-                # Redirige a la página de login si el registro fue exitoso
-                return redirect('login')
-            else:
-                # Si ocurre un error en la API, muestra el mensaje de error
-                return JsonResponse({'error': response.text}, status=response.status_code)
-
-        except requests.exceptions.RequestException as e:
-            # Maneja los errores de conexión o problemas con la solicitud
-            print(f"Error al registrar el usuario: {e}")
-            return JsonResponse({'error': f"Error al registrar el usuario: {str(e)}"}, status=400)
-        
-    return render(request, 'register.html')
 
 def monitoring_view(request):
      return render(request, 'monitoring.html')
@@ -123,6 +81,7 @@ def obtener_ubicacion_vehiculo(request):
     else:
         data = {"error": "No se encontraron datos de ubicación."}
     return JsonResponse(data)
+
 def getAPI(request):
      URL_API = "https://apitesis.fly.dev/api/v1/vehiculo/"
      
